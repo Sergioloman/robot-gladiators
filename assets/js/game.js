608ac1db99ba14dda56e60176b854cf2ea0c7a8e
Sergio Torres <sergio.lo.torres@gmail.com>
2021-02-06 18:40:14 -0600

    Robots are able to figtht each other

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,55 @@
-window.alert("this is an alert! JavaScript is Running!");
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+//you can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roboto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+//fight function
+var fight = function() {
+    //alert players 
+    window.alert("welcome to Robot Gladiators!");
+
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+    // Log a resulting message to the console so we know that it worked.
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked.
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
+//check enemy's health
+
+if (enemyHealth <=0){
+    window.alert(enemyName + " has died!");
+}
+else{
+    window.alert(enemyName + " still has " + enemyHealth +" health left.");  
+}
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
+enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+if (playerHealth <=0){
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+};
+//end fight function
+
+fight ();
+
